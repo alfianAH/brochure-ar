487fa81b40a51df42aac9233317fbd2f64195437
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-11-21 15:09:56 +0800

    Check and swipe house

---
+++ Assets/Scripts/CheckHouse.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckHouse : MonoBehaviour
+{
+    [SerializeField] private SwipeHouse swipeHouse;
+    [SerializeField] private Button leftButton, rightButton;
+    [SerializeField] private Text houseTypeText;
+    
+    private int startHouse, lastHouse;
+
+    private void Start()
+    {
+        startHouse = 0;
+        lastHouse = swipeHouse.houses.Length - 1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If other is start house, ...
+        if (other.gameObject == swipeHouse.houses[startHouse])
+        {
+            // Make left button's interactable to false
+            leftButton.interactable = false;
+        }
+        else if(other.gameObject == swipeHouse.houses[lastHouse])
+        {
+            // Make rigtht button's interactable to false
+            rightButton.interactable = false;
+        }
+        else
+        {
+            leftButton.interactable = true;
+            rightButton.interactable = true;
+        }
+
+        houseTypeText.text = other.name;
+    }
+}
---
+++ Assets/Scripts/SwipeHouse.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SwipeHouse : MonoBehaviour
+{
+    public GameObject[] houses;
+
+    public void SwipeRight()
+    {
+        foreach (var house in houses)
+        {
+            house.transform.position =
+                new Vector3(house.transform.position.x - 50f,
+                    house.transform.position.y, 
+                    house.transform.position.z);
+        }
+    }
+
+    public void SwipeLeft()
+    {
+        foreach (var house in houses)
+        {
+            house.transform.position =
+                new Vector3(house.transform.position.x + 50f,
+                    house.transform.position.y, 
+                    house.transform.position.z);
+        }
+    }
+}
